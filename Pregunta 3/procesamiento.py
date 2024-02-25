6bd40150bd29399117ad5b2c2bd753fcb64f6a52
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-02-25 01:17:41 +0000

    pc5

--- Pregunta 3/procesamiento.py
+++ Pregunta 3/procesamiento.py
@@ -60,4 +60,82 @@ if __name__ == "__main__":
     # Guardar el DataFrame procesado en un nuevo archivo Excel
     df.to_excel('reactiva_procesado.xlsx', index=False)
 
+
+# GENERANDO REPORTES USANDO DE EJ SQLLITE
+    import sqlite3
+
+# ... (código anterior)
+
+def guardar_en_base_de_datos(df):
+    # Crear o conectar a la base de datos SQLite
+    conexion = sqlite3.connect('ubigeos.db')
+
+    # Guardar la tabla de ubigeos en la base de datos
+    df[['ubigeo', 'region', 'provincia', 'distrito']].drop_duplicates().to_sql('ubigeos', conexion, index=False, if_exists='replace')
+
+    # Cerrar la conexión a la base de datos
+    conexion.close()
+
+def generar_reportes_por_region(df):
+    # Agrupar por región y tipo de obra
+    grupos = df.groupby(['region', 'tipoobra', 'estado'])
+
+    for (region, tipo_obra, estado), grupo in grupos:
+        if tipo_obra == 'Urbano':
+            # Filtrar por las 5 mayores inversiones
+            top5 = grupo.nlargest(5, 'montodeinversion')
+
+            # Verificar si hay datos en el top5
+            if not top5.empty:
+                # Generar el nombre del archivo Excel
+                nombre_archivo = f'{region}_top5_{tipo_obra}_estado_{estado}.xlsx'
+
+                # Guardar el top5 en un archivo Excel
+                top5.to_excel(nombre_archivo, index=False)
+
+if __name__ == "__main__":
+    # ... (código anterior)
+
+    # Guardar en la base de datos
+    guardar_en_base_de_datos(df)
+
+    # Generar reportes por región y tipo de obra
+    generar_reportes_por_region(df)
+
+    # GENERACIÓN DE ENVÍO DE CORREO
+    import os 
+
+
+
+smtp_server = 'smtp.gmail.com'  # Cambia esto al servidor SMTP que estés utilizando
+smtp_port = 587  # Cambia esto al puerto adecuado
+sender_email = 'leonleonel035@gmail.com'
+sender_password = open('token.txt').read().strip()
+
+# Detalles del correo electrónico
+receiver_email = 'carolinaccorisapra002@gmail.com'
+subject = 'Envio Reporte reactiva'
+body = 'Adjunto lo solicitado'
+
+# Crear el objeto MIMEMultipart
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = receiver_email
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+
+
+# Adjuntar archivo
+file_path = './data/reactiva.xlsx'  # Cambia la ruta al archivo que quieras adjuntar
+with open(file_path, 'rb') as file:
+    attachment = MIMEApplication(file.read(), _subtype="xlsx")
+    attachment.add_header('Content-Disposition', 'attachment', filename=file_path)
+    msg.attach(attachment)
     
+# Iniciar la conexión con el servidor SMTP
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.starttls()  # Iniciar el modo seguro
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+
+print('Correo enviado exitosamente')
